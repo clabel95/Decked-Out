d22cd7a368aca067951b3a2436e2313173b14f08
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-13 14:11:50 -0500

    first pass at models

--- server/models/Deck.js
+++ server/models/Deck.js
@@ -1,3 +1,33 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./User');
+const flashcardSchema = require('./Flashcard');
+
+const deckSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        date_created: {
+            type: Date,
+            default: Date.now(),
+        },
+        author: userSchema,
+        flashcards: [flashcardSchema],
+    }
+);
+
+const Deck = model('deck', deckSchema);
+
+module.exports = Deck;
+
 //title
 //category
 //description
