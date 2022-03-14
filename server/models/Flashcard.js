06a6c7352ef764f96661bde24879387b3b786450
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-14 13:21:42 -0500

    fixed Flashcard model

--- server/models/Flashcard.js
+++ server/models/Flashcard.js
@@ -16,38 +16,6 @@ const flashcardSchema = new Schema(
         noteSideB: {
             type: String,
         },
-        deck: deckSchema,
-    }
-);
-
-const Flashcard = model('flashcard', flashcardSchema);
-
-module.exports = Flashcard;
-
-//keys and values
-//sideA
-//sideB
-//optional noteSideA
-//optional noteSideB
-
-const { Schema, model } = require('mongoose');
-
-const flashcardSchema = new Schema(
-    {
-        sideA: {
-            type: String,
-            required: true,
-        },
-        sideB: {
-            type: String,
-            required: true,
-        },
-        noteSideA: {
-            type: String,
-        },
-        noteSideB: {
-            type: String,
-        },
         deck: [
             {
                 type: Schema.Types.ObjectId,
