79f6b49a0fcca5f545c4d22dcf6adc6f4b08773f
Benjamin Gomez <3gobenjamin@gmail.com>
2022-03-14 13:10:26 -0500

    good stuff

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+// import { Link } from 'react-router-dom'
 import highlighterLogin from '../Assets/highlighterLogin.png';
 import highlighterSignUp from '../Assets/highlighterSignUp.png';
 
-import SignUp from './SignUp';
+import SignUp from '../SignUp/SignUp';
 import { useState } from 'react';
 import "./landing.css";
 
@@ -27,6 +27,36 @@ import "./landing.css";
 // }
 
 function Landing() {
+    // const [formState, setFormState] = useState({
+    //     username: '',
+    //     email: '',
+    //     password: '',
+    // });
+    // // const [addUser, { error, data }] = useMutation(ADD_USER);
+
+    // const handleChange = (event) => {
+    //     const { name, value } = event.target;
+
+    //     setFormState({
+    //         ...formState,
+    //         [name]: value,
+    //     });
+    // };
+
+    // const handleFormSubmit = async (event) => {
+    //     event.preventDefault();
+    //     console.log(`this is the state of the form ${formState}`)
+
+    //     try {
+    //         const { data } = await addUser({
+    //             variables: { ...formState },
+    //         });
+
+    //         Auth.login(data.addUser.token);
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // }
     const [buttonPopup, setButtonPopup] = useState(false)
     return (
         <div>
@@ -42,11 +72,11 @@ function Landing() {
 
                 
 
-                <div id="highlightSignUp">
+                 <div id="highlightSignUp">
                     <img id="highlit" src={highlighterSignUp} />
                     <div id="signUp" onClick={() => setButtonPopup(true)}> Sign Up </div>
-                    <SignUp trigger={buttonPopup} setTrigger={setButtonPopup}>
-                        <form >
+                     <SignUp trigger={buttonPopup} setTrigger={setButtonPopup}/>
+                        {/* <form >
                             <label id="formSignUp">
                                 Email:............ .....
                                 <input type="text"/>
@@ -58,10 +88,10 @@ function Landing() {
                                 <input type="text"/>
                             </label>
                             
-                        </form>
-                    </SignUp>
+                        </form> */}
+                    {/* </SignUp>  */}
                     
-                </div>
+                </div> 
             </div>
         </div>
     );
