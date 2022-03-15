172bf9d157d84b632290d00aaedcc1df9b350055
ByrdBassPC <byrdbass@gmail.com>
2022-03-15 13:30:26 -0500

    fixed errors in typeDefs to match the resolvers, may need to further correct the keys and values for Mutations in typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,125 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Deck, Flashcard, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    decks: async () => {
+        //find all the decks for the homepage and populate the JUST THE DECKS
+        return await Deck.find({});//.populate('flashcards');
+    },
+    //populate one flashcard at a time from the corresponding deck by id
+    //DO WE HAVE TO LOOP THROUGH THE OTHER FLASHCARDS SOMEHOW
+    flashcard: async (parent, { category }) => {
+      return await Deck.findById(_id);
+    },
+    //find the user by ID, and populate flashcards and decks at the same time
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'decks',
+          populate: 'flashcards'
+        });
+        return user;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+  Mutation: {
+    //this is creating a user from the sign up page
+  addUser: async (parent, args) => {
+    const user = await User.create(args);
+    const token = signToken(user);
+
+    return { token, user };
+  },
+  //adding a deck from the navbar on the side = on the plus sign on page 6 of wireframe, component is deck_create, associated with next button
+  addDeck: async (parent, args) => {
+      const deck = await Deck.create(args);
+      return deck;
+  },
+  // this will be the 'plus' on the same 'next' page from the same component as the previous one
+  addFlashCard: async (parent, args) => {
+      const flashcard = await Flashcard.create(args);
+      return flashcard;
+  },
+  // NOT NECESSARILY FUNCTIONING IN THE CURRENT SETUP - OPTIONAL
+  updateUser: async (parent, args, context) => {
+    if (context.user) {
+      return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+    }
+    throw new AuthenticationError('Not logged in');
+  },
+  //appends information to the deck, title, category, description should be called from slide 5 of wireframe
+  updateDeck: async (parent, args, context) => {
+      if (context.deck) {
+          return await Deck.findByIdAndUpdate(context.deck._id, args, {new: true});
+      }
+  },
+  //same thing as previous but updating a flashcard - NOT NECESSARILY BEING FUNCTIONAL WITH CURRENT WIREFRAME SETUP
+  updateFlashCard: async (parent, args, context) => {
+      if (context.deck) {
+          return await Flashcard.findByIdAndUpdate(context.flashcard._id, args, {new: true});
+      }
+  },
+  // USER IS BEING FOUND BY EMAIL (NO REQUEST FOR USERNAME CURRENTLY BEING USED) 
+  // this doesn't exit yet - PASSWORD MUST BE LONGER THAN 8 CHARACTERS TO WORKS
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+
+    const correctPw = await user.isCorrectPassword(password);
+
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+
+    const token = signToken(user);
+
+    return { token, user };
+  }
+}
+};
+
+
+    // If we wanted to include Stripe payment processing
+//--------------------------------------------------------------
+//     checkout: async (parent, args, context) => {
+//       const url = new URL(context.headers.referer).origin;
+//       const order = new Order({ products: args.products });
+//       const line_items = [];
+
+//       const { products } = await order.populate('products');
+
+//       for (let i = 0; i < products.length; i++) {
+//         const product = await stripe.products.create({
+//           name: products[i].name,
+//           description: products[i].description,
+//           images: [`${url}/images/${products[i].image}`]
+//         });
+
+//         const price = await stripe.prices.create({
+//           product: product.id,
+//           unit_amount: products[i].price * 100,
+//           currency: 'usd',
+//         });
+
+//         line_items.push({
+//           price: price.id,
+//           quantity: 1
+//         });
+//       }
+//       return { session: session.id };
+//     }
+// -----------------------------------------------------------------------
+
+
+
+  //Still functional for the most part
+
+
+module.exports = resolvers;
+
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,58 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Deck {
+    title: String
+    category: String
+    description: String
+    author: ID
+    date_created: String
+    flashcards: [Flashcard]
+  }
+  type Flashcard {
+    _id: ID
+    sideA: String
+    sideB: String
+    noteSideA: String
+    noteSideB: String
+    deck: [Deck]
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    decks: [Deck]
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+  type Query {
+    decks: [Deck]
+    flashcards(category: ID, name: String): [Flashcard]
+    flashcard(_id: ID!): Flashcard
+    user: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addDeck(deck: [ID]!): Deck
+    addFlashCard(
+      sideA: String!
+      sideB: String!
+      noteSideA: String 
+      noteSideB: String): Flashcard
+    updateFlashCard(sideA: String!, sideB: String!, noteSideA: String, noteSideB: String): Flashcard
+    updateUser(username: String, email: String, password: String): User
+    updateDeck(title: String!, category: String!, description: String): Deck
+    login(email: String!, username: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
+
+// Card category/deck
+// id
+// name string
+
+//
