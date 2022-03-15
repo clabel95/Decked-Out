6723e4ee875282ad51f0b0bb4d2930a8d43d9921
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-15 12:52:53 -0500

    test html for deck_create

---
+++ client/src/components/Carousel_Test/carousel_test.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge: "right"});
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
