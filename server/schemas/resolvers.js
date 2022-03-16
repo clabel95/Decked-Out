fad24cbab62d3d770c097d0b51c59049ce3d8ad9
Benjamin Gomez <3gobenjamin@gmail.com>
2022-03-16 18:08:42 -0500

    changed up login and sign up button formatting to allow user data to be sent to mongo db and send us to home page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -69,8 +69,8 @@ const resolvers = {
   },
   // USER IS BEING FOUND BY EMAIL (NO REQUEST FOR USERNAME CURRENTLY BEING USED) 
   // this doesn't exit yet - PASSWORD MUST BE LONGER THAN 8 CHARACTERS TO WORKS
-  login: async (parent, { email, password }) => {
-    const user = await User.findOne({ email });
+  login: async (parent, { username, password }) => {
+    const user = await User.findOne({ username });
 
     if (!user) {
       throw new AuthenticationError('Incorrect credentials');
