6d072b56ec6f1e2c7dd0ed96240f6294f173344b
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-16 10:27:39 -0500

    added login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,17 @@
 import { gql } from "@apollo/client";
 
+export const LOGIN_USER = gql`
+    mutation login($username: String!, $password: String!) {
+        login (username: $username, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
 export const  ADD_USER = gql`
     mutation addUser($username: String, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
