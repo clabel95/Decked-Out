80aa900b909973afd5f7a2b550887b69c7ffa4e5
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-16 21:00:13 -0500

    search bar panel now displays

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./initalize.js";
 
+import M from "materialize-css";
 
-
-function Search_Bar(props) {
-    // not sure how we want to set this up.
-    // are we going to have set categories and just hard code the values into the options below?
-    // or are we going to dynamicly add options to the drop down menu?
-    const options = []
-    for (var i = 0; i < categories.lenght; i +=1){
-        options.push(<option value={i}>{props.categories[i]}</option>)
+class SearchBar extends Component {
+    componentDidMount() {
+        const opt = {
+            edge: "right"
+        };
+        // M.SearchBar.init(this.SearchBar, opt);
+        M.AutoInit();
     }
-
-    return (
-        <div>
-            <ul id="slide-out" class="sidenav">
-                <li>
-                    <div class="row">
-
-                        <i class="col s2 material-icons" style="font-size:45px">search</i>
-                        <div class="input-field col s10 ">
-                            <input placeholder="Search" type="text" class="validate" />
+    
+    render() {
+        const options = []
+        for (var i = 0; i < this.props.categories.length; i +=1){
+            options.push(<option value={this.props.categories[i]}>{this.props.categories[i]}</option>)
+        }
+        return (
+            <div>
+                <ul id="slide-out" className="sidenav">
+                    <li>
+                        <div className="row">
+    
+                            {/* <i className="col s2 material-icons" style="font-size:45px">search</i> */}
+                            <div className="input-field col s10 ">
+                                <input placeholder="Search" type="text" className="validate" />
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <select>
-                                <option value="" disabled selected>Categories</option>
-                                <option value="1">Sports</option>
-                                <option value="2">Pokemon</option>
-                                <option value="3">Games</option>
-                                {options}
-                            </select>
+                    </li>
+                    <li>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <select>
+                                    <option value="" disabled selected>Categories</option>
+                                    {options}
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="row">
-                        <button class="btn waves-effect waves-light col s8 offset-s2" type="submit" name="action">Search
-                            <i class="material-icons right">send</i>
-                        </button>
-                    </div>
-                </li>
-            </ul>
-            <a href="#" data-target="slide-out" class="sidenav-trigger right"><i class="material-icons">search</i></a>
-            <script src="./initalize.js"></script>
-        </div>
-    );
+                    </li>
+                    <li>
+                        <div className="row">
+                            <button className="btn waves-effect waves-light col s8 offset-s2" type="submit" name="action">Search
+                                <i className="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+                <a href="#" data-target="slide-out" className="sidenav-trigger right"><i className="material-icons">search</i></a>
+                <script src="./initalize.js"></script>
+            </div>
+        );
+    }
+    
 }
 
-export default Search_Bar;
+export default SearchBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,17 +4,19 @@ import React from 'react';
 // import Footer from "../components/Footer_WIP/Footer";
 // import NavBar from "../components/NavBar/NavBar";
 import Carousel_Home from "../components/Carousel_Home/index";
-// import SearchBar from "../components/SearchBar/index";
+import SearchBar from "../components/SearchBar/index";
 
 
 const Home = () => {
+  const categories = ["Sports", "Pokemon", "Games"];
+
   return (
     <main>
       <div className="flex-row justify-center">
         <div>
           <h2>Hello</h2>
           <Carousel_Home/>
-          {/* <SearchBar/> */}
+          <SearchBar categories={categories}/>
         </div>
       </div>
     </main>
